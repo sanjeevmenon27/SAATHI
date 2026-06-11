6bbc34e598558bee93a85f911b0ac85e3502b29b
sanjeevmenon27 <sanjeevsmenon27@gmail.com>
2026-06-11 14:33:43 +0530

    perf: set MongoDB connection timeout to 3s to prevent server hang

--- server/src/db.js
+++ server/src/db.js
@@ -5,7 +5,7 @@ let dbConnected = false;
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(config.mongoUri);
+    await mongoose.connect(config.mongoUri, { serverSelectionTimeoutMS: 3000 });
     dbConnected = true;
     console.log("Connected to MongoDB successfully");
     return true;
